dad5be248b42b0228b1e6fc1bfcd857c77668bd1
Frank Tackitt <franklyn@tackitt.net>
2014-02-19 15:36:22 -0700

    Add search options and search for multiple values at once

--- mame/views/search.py
+++ mame/views/search.py
@@ -3,7 +3,7 @@ from wtforms.fields.core import RadioField
 __author__ = 'frank'
 from .. import app, db, models, redirect, render_template, request, abort, url_for
 from flask.ext.wtf import Form
-from wtforms.fields import TextField, SelectField, SelectMultipleField, BooleanField, SubmitField, HiddenField
+from wtforms.fields import TextField, SelectMultipleField, SelectMultipleField, BooleanField, SubmitField, HiddenField, SelectField
 from wtforms.validators import Optional, Required, InputRequired
 
 #values for searching go here
@@ -13,6 +13,7 @@ enum_dump_status = [(c,c) for c in ["", "baddump", "nodump", "good"]]
 enum_savestate = [(c,c) for c in ["", "supported", "unsupported"]]
 years = [(c,c) for c in ["", "????", "19??", "1971", "1972", "1974", "1975", "1976", "1977", "1978", "1979", "1979?", "198?", "1980", "1980?", "1981", "1981?", "1982", "1982?", "1983", "1983?", "1984", "1984?", "1985", "1986", "1986?", "1987", "1987?", "1988", "1988?", "1989", "1989?", "199?", "1990", "1990?", "1991", "1991?", "1992", "1992?", "1993", "1993?", "1994", "1994?", "1995", "1995?", "1996", "1996?", "1997", "1998", "1999", "1999?", "200?", "2000", "2000?", "2001", "2002", "2002?", "2003", "2004", "2004?", "2005", "2005?", "2006", "2007", "2007?", "2008", "2009", "2010"]]
 enum_control_type = [(c,c) for c in ["", "joy", "paddle", "gambling", "pedal", "keypad", "hanafuda", "lightgun", "trackball", "positional", "dial", "doublejoy", "mouse", "stick", "mahjong", "keyboard"]]
+enum_screen_rotation = [(c,c) for c in ['0', '90', '180', '270']]
 
 class SearchForm(Form):
 
@@ -20,28 +21,29 @@ class SearchForm(Form):
     description = TextField("Description", validators=[Optional()], default='')
     manufacturer = TextField("Manufacturer", validators=[Optional()], default='')
 
-    year = SelectField("Year", choices=years, validators=[Optional()], default='')
+    year = SelectMultipleField("Year", choices=years, validators=[Optional()], default='')
 
     type = SelectField("ROM Type", validators=[Optional()], choices=[(c,c) for c in ['All', 'Bios', 'Device', 'Mechanical']])
 
-    cloneof = SelectField("Show Clones?", default='Yes', validators=[Optional()], choices=[('Yes', 'Yes'), ('No', 'No')])
+    cloneof = SelectMultipleField("Show Clones?", default='Yes', validators=[Optional()], choices=[('Yes', 'Yes'), ('No', 'No')])
 
-    rom_status = SelectField("Rom Status", choices=enum_dump_status, validators=[Optional()], default='')
+    rom_status = SelectMultipleField("Rom Status", choices=enum_dump_status, validators=[Optional()], default='')
     
-    status = SelectField("Overall Status", choices=enum_status, validators=[Optional()], default='')
-    emulation = SelectField("Emulation Status", choices=enum_status, validators=[Optional()], default='')
-    color = SelectField("Color Status", choices=enum_status, validators=[Optional()], default='')
-    sound = SelectField("Sound Status", choices=enum_status, validators=[Optional()], default='')
-    graphic = SelectField("Graphics Status", choices=enum_status, validators=[Optional()], default='')
-    cocktail = SelectField("Cocktail Status", choices=enum_status, validators=[Optional()], default='')
-    protection = SelectField("Protection Status", choices=enum_status, validators=[Optional()], default='')
-    savestate = SelectField("SaveState Status", choices=enum_savestate, validators=[Optional()], default='')
+    status = SelectMultipleField("Overall Status", choices=enum_status, validators=[Optional()], default='')
+    emulation = SelectMultipleField("Emulation Status", choices=enum_status, validators=[Optional()], default='')
+    color = SelectMultipleField("Color Status", choices=enum_status, validators=[Optional()], default='')
+    sound = SelectMultipleField("Sound Status", choices=enum_status, validators=[Optional()], default='')
+    graphic = SelectMultipleField("Graphics Status", choices=enum_status, validators=[Optional()], default='')
+    cocktail = SelectMultipleField("Cocktail Status", choices=enum_status, validators=[Optional()], default='')
+    protection = SelectMultipleField("Protection Status", choices=enum_status, validators=[Optional()], default='')
+    savestate = SelectMultipleField("SaveState Status", choices=enum_savestate, validators=[Optional()], default='')
 
-    buttons = SelectField("Buttons", choices=[("","")] + [(str(c),str(c)) for c in range(1,18)], validators=[Optional()], default='')
-    players = SelectField("Players", choices=[("","")] + [(str(c),str(c)) for c in range(0,8)], validators=[Optional()], default='')
+    buttons = SelectMultipleField("Buttons", choices=[("","")] + [(str(c),str(c)) for c in range(1,18)], validators=[Optional()], default='')
+    players = SelectMultipleField("Players", choices=[("","")] + [(str(c),str(c)) for c in range(0,8)], validators=[Optional()], default='')
 
-    control_type = SelectField("Control Type", choices=enum_control_type, validators=[Optional()], default='')
+    control_type = SelectMultipleField("Control Type", choices=enum_control_type, validators=[Optional()], default='')
 
+    rotation = SelectMultipleField("Screen Rotation", choices=enum_screen_rotation, validators=[Optional()], default='')
 
 @app.route("/search")
 @app.route("/search/<int:page>")
@@ -78,8 +80,8 @@ def search(page=1):
 
     query = models.Game.query
 
-    if form.year.data != '':
-        query = query.filter(models.Game.year == form.year.data)
+    if len(form.year.data):
+        query = query.filter(models.Game.year.in_(form.year.data))
     if form.name.data != '':
         query = query.filter(models.Game.name.ilike("%%%s%%" % form.name.data))
     if form.description.data != '':
@@ -95,33 +97,35 @@ def search(page=1):
             query = query.filter(models.Game.isdevice == 'yes')
         elif form.type.data == 'Mechanical':
             query = query.filter(models.Game.ismechanical == 'yes')
-    if form.status.data != '':
-        query = query.filter(models.Game.status == form.status.data)
+    if len(form.status.data):
+        query = query.filter(models.Game.status.in_(form.status.data))
 
-    if form.emulation.data != '':
-        query = query.filter(models.Game.emulation == form.emulation.data)
-    if form.color.data != '':
-        query = query.filter(models.Game.color == form.color.data)
-    if form.sound.data != '':
-        query = query.filter(models.Game.sound == form.sound.data)
-    if form.graphic.data != '':
-        query = query.filter(models.Game.graphic == form.graphic.data)
-    if form.cocktail.data != '':
-        query = query.filter(models.Game.cocktail == form.cocktail.data)
-    if form.protection.data != '':
-        query = query.filter(models.Game.protection == form.protection.data)
-    if form.savestate.data != '':
-        query = query.filter(models.Game.savestate == form.savestate.data)
-    if form.buttons.data != '':
-        query = query.filter(models.Game.buttons == form.buttons.data)
-    if form.players.data != '':
-        query = query.filter(models.Game.players == form.players.data)
+    if len(form.emulation.data):
+        query = query.filter(models.Game.emulation.in_(form.emulation.data))
+    if len(form.color.data):
+        query = query.filter(models.Game.color.in_(form.color.data))
+    if len(form.sound.data):
+        query = query.filter(models.Game.sound.in_(form.sound.data))
+    if len(form.graphic.data):
+        query = query.filter(models.Game.graphic.in_(form.graphic.data))
+    if len(form.cocktail.data):
+        query = query.filter(models.Game.cocktail.in_(form.cocktail.data))
+    if len(form.protection.data):
+        query = query.filter(models.Game.protection.in_(form.protection.data))
+    if len(form.savestate.data):
+        query = query.filter(models.Game.savestate.in_(form.savestate.data))
+    if len(form.buttons.data):
+        query = query.filter(models.Game.buttons.in_(form.buttons.data))
+    if len(form.players.data):
+        query = query.filter(models.Game.players.in_(form.players.data))
 
     #nasty subselects...
-    if form.control_type.data != '':
-        query = query.filter(models.Game.control.any(models.Control.type==form.control_type.data))
-    if form.rom_status.data != '':
-        query = query.filter(models.Game.rom.any(models.Rom.status==form.rom_status.data))
+    if len(form.control_type.data):
+        query = query.filter(models.Game.control.any(models.Control.type.in_(form.control_type.data)))
+    if len(form.rom_status.data):
+        query = query.filter(models.Game.rom.any(models.Rom.status.in_(form.rom_status.data)))
+    if len(form.rotation.data):
+        query = query.filter(models.Game.display.any(models.Display.rotate.in_(form.rotation.data)))
 
     results = query.paginate(page=page, per_page=25)
 
